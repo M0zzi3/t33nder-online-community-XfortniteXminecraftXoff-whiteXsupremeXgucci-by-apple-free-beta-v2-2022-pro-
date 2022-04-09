f99fea66a929bd6be7feaa51df23096d55683c04
M0zzi3 <notmozzi3>
2022-04-09 20:00:31 +0200

    added login and logout functionality

--- app.py
+++ app.py
@@ -1,13 +1,50 @@
-from flask import Flask, render_template, request, make_response
+from datetime import timedelta
+
+from flask import Flask, render_template, request, make_response, redirect, url_for, session
 import random
 
 
 app = Flask(__name__)
+app.secret_key = "qazxdr4321"
+app.permanent_session_lifetime = timedelta(minutes=2)
 
 
 @app.route('/')
 def home_page():
-    return render_template('home.html')
+    if "user" in session:
+        return render_template('home.html')
+    else:
+        return redirect(url_for("login_page"))
+
+
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login_page():
+    if request.method == "POST":
+        session.permanent = True
+        user_name = request.form["user_name"]
+        session["user"] = user_name
+        return redirect(url_for("user_page"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user_page"))
+        return render_template('login.html')
+
+
+@app.route('/user')
+def user_page():
+    if "user" in session:
+        user_name = session['user']
+        return render_template('user.html', data=user_name)
+    else:
+        return redirect(url_for("login_page"))
+
+
+@app.route("/logout")
+def logout_page():
+    session.pop("user", None)
+    return redirect(url_for("login_page"))
 
 
 
@@ -16,11 +53,6 @@ def cookie_page():
     response = make_response('<h1>Ten dokument zawiera plik cookie!</h1>')
     response.set_cookie('odpowiedz', '42')
     return response
-
-
-@app.route('/user/<user_name>')
-def user_page(user_name):
-    return render_template('user.html', data=user_name)
 
 @app.errorhandler(404)
 def page_not_found(e):
